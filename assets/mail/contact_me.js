1f086e718b7ca46a0310500e19d61574a95f1ad7
Mojocoder <84145874+Mojocoder@users.noreply.github.com>
2021-07-04 21:26:35 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{https://eax7yy8db9.execute-api.eu-west-1.amazonaws.com/Portfolio_lambda}",
+                url: "{https://tdwxvq10sk.execute-api.eu-west-1.amazonaws.com/Portfolio_lambda}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
