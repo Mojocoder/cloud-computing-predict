026982c932515233e5162a95b8740fb20c237b2e
Mojocoder <84145874+Mojocoder@users.noreply.github.com>
2021-07-05 23:02:46 +0200

    Update aggregated_lambda_function.py

--- student_solution_files/aggregated_lambda_function.py
+++ student_solution_files/aggregated_lambda_function.py
@@ -93,7 +93,7 @@ def lambda_handler(event, context):
     # Replace recipient@example.com with a "To" address. If your account
     # is still in the sandbox, this address must be verified.
     # --- Insert your code here ---
-    RECIPIENT = 'info.iceprojects1@gmail.com'
+    RECIPIENT = 'kevinreddyo@yahoo.com'
     # -----------------------------
 
     AWS_REGION = "eu-west-1"
